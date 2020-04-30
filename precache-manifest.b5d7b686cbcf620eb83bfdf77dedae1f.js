e92a7efe8b9ed86f97157f150ae25c58012cf797
Prasannarajmallipudi <prasannarajmallipudi@gmail.com>
2020-04-30 09:26:44 +0530

    Updates

---
+++ precache-manifest.b5d7b686cbcf620eb83bfdf77dedae1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc3b9b3b35697cf1b5777dedd1376c4",
+    "url": "/reactprofile/index.html"
+  },
+  {
+    "revision": "9f6520a7f5dd51c6e0d9",
+    "url": "/reactprofile/static/css/main.c0d663da.chunk.css"
+  },
+  {
+    "revision": "c41ac96aacb47c4afe4a",
+    "url": "/reactprofile/static/js/2.4696e22c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactprofile/static/js/2.4696e22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f6520a7f5dd51c6e0d9",
+    "url": "/reactprofile/static/js/main.40805883.chunk.js"
+  },
+  {
+    "revision": "9f4a3aa8cae86a13aa04",
+    "url": "/reactprofile/static/js/runtime-main.10a81835.js"
+  }
+]);
